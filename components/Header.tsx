0c9db644b60c1ed768daa1a2b9c667fce521bf8b
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2021-02-21 16:57:41 +0330

    Change components into chakra ones

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import styles from '../styles/Home.module.css';
-import Button from './Button';
-import TextField from './TextField';
+import { Button, Input } from '@chakra-ui/react';
 
 const QUERY = gql`
   query FetchTodos {
@@ -18,18 +18,32 @@ const Root = styled.header`
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
-  height: 200px;
+  width: 320px;
+  height: 240px;
 `;
 
 const Header = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { data, error, loading } = useQuery(QUERY);
   console.log(data);
+
+  const handleClickGenerate = () => {
+    setIsLoading(true);
+  };
 
   return (
     <Root>
       <h1 className={styles.title}>Miten!</h1>
-      <TextField placeholder="Your Email..." />
-      <Button label="Generate a Miten!" />
+      <Input placeholder="Your Email..." />
+      <Button
+        isLoading={isLoading}
+        loadingText="Generating..."
+        colorScheme="teal"
+        onClick={handleClickGenerate}
+      >
+        Generate a Miten!
+      </Button>
     </Root>
   );
 };
