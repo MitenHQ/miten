aa5fddf20ae7f2ef51b9f8fcf26d2834d57680ea
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2021-02-22 23:52:07 +0330

    Remove copy boxes to prevent spam emails

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -12,7 +12,7 @@ const Root = styled.header`
   height: 480px;
 `;
 
-const LinkContainer = styled.section`
+const MessageContainer = styled.section`
   display: flex;
   flex-flow: column nowrap;
   width: 320px;
@@ -20,7 +20,7 @@ const LinkContainer = styled.section`
   color: teal;
   text-align: center;
   & span {
-    font-size: 0.6rem;
+    font-size: 0.8rem;
   }
 `;
 
@@ -28,29 +28,15 @@ const Header: FC = () => {
   const [generateLink, { loading, data, error }] = useGenerateLinkMutation({
     errorPolicy: 'all',
   });
-  console.log(data, error);
 
   const [email, setEmail] = useState('');
-  const [isFeedBackLinkCopied, setIsFeedBackLinkCopied] = useState(false);
-  const [isReportLinkCopied, setIsReportLinkCopied] = useState(false);
 
   const handleChangeEmail = (event: any) => {
     setEmail(event.target.value);
   };
+
   const handleClickGenerate = () => {
     generateLink({ variables: { data: { email } } });
-  };
-
-  const handleCopyLink = (e: any) => {
-    e.target.select();
-    document.execCommand('copy');
-    if (e.target.name === 'feedbackLinkInput') {
-      setIsFeedBackLinkCopied(true);
-      setIsReportLinkCopied(false);
-    } else {
-      setIsReportLinkCopied(true);
-      setIsFeedBackLinkCopied(false);
-    }
   };
 
   return (
@@ -65,28 +51,9 @@ const Header: FC = () => {
       >
         Generate a Link!
       </Button>
-      <LinkContainer>
-        {data && (
-          <>
-            <h2 className={styles.subtitle}>Feedback Link</h2>
-            <span>{isFeedBackLinkCopied ? 'COPIED :)' : 'CLICK LINK TO COPY'}</span>
-            <Input
-              name="feedbackLinkInput"
-              onClick={handleCopyLink}
-              value={data.generateLink?.link ?? ''}
-              onChange={() => null}
-            />
-            <h2 className={styles.subtitle}>Get Reports</h2>
-            <span>{isReportLinkCopied ? 'COPIED :)' : 'CLICK LINK TO COPY'}</span>
-            <Input
-              name="reportLinkInput"
-              onClick={handleCopyLink}
-              value={data.generateLink?.reportLink ?? ''}
-              onChange={() => null}
-            />
-          </>
-        )}
-      </LinkContainer>
+      <MessageContainer>
+        {data ? <span>The related links has been sent to your email.</span> : error}
+      </MessageContainer>
     </Root>
   );
 };
