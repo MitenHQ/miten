e976d426d64b654fe6ccac4aedc695d76fc8f56f
albacoretuna <7697632+albacoretuna@users.noreply.github.com>
2021-02-25 22:00:23 +0200

    fix lint errors

--- backend/src/feedback/generateLink.ts
+++ backend/src/feedback/generateLink.ts
@@ -1,17 +1,19 @@
 import { MutationResolvers } from '../graphql/resolvers-types';
 import { getOrMakeUser } from '../user/getOrMakeUser';
-import { uniqueId } from '../utils/uniqueId'
+import { uniqueId } from '../utils/uniqueId';
 
-export const generateLink: MutationResolvers['generateLink'] = async (parent, {data:{email}}, {prisma}) => {
-  const user = await getOrMakeUser({email})
+export const generateLink: MutationResolvers['generateLink'] = async (
+  parent,
+  { data: { email } },
+  { prisma },
+) => {
+  const user = await getOrMakeUser({ email });
 
-
-  const response = await prisma.feedbackBase.create(
-    {data:
-      {uid: uniqueId(), user: {connect:{id:user.id}}}
-    }
-  )
+  await prisma.feedbackBase.create({
+    data: { uid: uniqueId(), user: { connect: { id: user.id } } },
+  });
   return {
-    success: true
-  }
+    // TODO add error handling
+    success: true,
+  };
 };
--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,14 +1,22 @@
 import React, { useState, FC, FormEvent } from 'react';
 import { useGenerateLinkMutation } from '../lib/graphql/hooks';
-import styles from '../styles/Home.module.css';
-import { Button, Container, Heading, Input, FormControl, FormHelperText, FormLabel, SimpleGrid } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  Heading,
+  Input,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  SimpleGrid,
+} from '@chakra-ui/react';
 import styled from 'styled-components';
 
 const Root = styled.header`
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
-  padding: 20px; 
+  padding: 20px;
   max-width: 90%;
 `;
 
@@ -34,33 +42,37 @@ const Header: FC = () => {
   };
 
   const handleSubmit = (event: FormEvent) => {
-    event.preventDefault(); if(!email) return; generateLink({ variables: { data: { email } } });
-  }
+    event.preventDefault();
+    if (!email) return;
+    generateLink({ variables: { data: { email } } });
+  };
 
   return (
     <Root>
       <Container>
-      <Heading as="h1" size="md" mb="7">Get a feedback form link!</Heading>
-      <form onSubmit={handleSubmit}>
-     <FormControl id="email">
-       <SimpleGrid spacing="10px">
-      <FormLabel mb="0">Your email</FormLabel>
-      <Input type="email" value={email} onChange={handleChangeEmail} />
-      <Button
-        type="submit"
-        isLoading={loading}
-        loadingText="Generating..."
-        colorScheme="teal"
-      >
-        Get a feedback link!
-      </Button>
-  <FormHelperText>We'll never share your email.</FormHelperText>
-       </SimpleGrid>
-</FormControl>   
-      </form>
-      <MessageContainer>
-        {data ? <span>The related links has been sent to your email.</span> : error}
-      </MessageContainer>
+        <Heading as="h1" size="md" mb="7">
+          Get a feedback form link!
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <FormControl id="email">
+            <SimpleGrid spacing="10px">
+              <FormLabel mb="0">Your email</FormLabel>
+              <Input type="email" value={email} onChange={handleChangeEmail} />
+              <Button
+                type="submit"
+                isLoading={loading}
+                loadingText="Generating..."
+                colorScheme="teal"
+              >
+                Get a feedback link!
+              </Button>
+              <FormHelperText>{'We will never share your email.'}</FormHelperText>
+            </SimpleGrid>
+          </FormControl>
+        </form>
+        <MessageContainer>
+          {data ? <span>The related links has been sent to your email.</span> : error}
+        </MessageContainer>
       </Container>
     </Root>
   );
