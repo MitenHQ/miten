d3b96b1f0a028c5b48dd8eeab2e5a7ecf1e4bda7
Pouria Maleki <dev.maleki@gmail.com>
2021-02-27 20:00:27 +0200

    more tests

--- backend/src/user/getOrMakeUser.test.ts
+++ backend/src/user/getOrMakeUser.test.ts
@@ -23,4 +23,15 @@ describe('getOrMakeUser', () => {
     const result = await getOrMakeUser(user);
     expect(result).toEqual(userRow);
   });
+
+  it('gets user correctly even with different char cases', async () => {
+    // first time make the user
+    const createdResult = await getOrMakeUser(user);
+    const userRow = await prisma.user.findUnique({ where: { email: user.email } });
+    expect(createdResult).toEqual(userRow);
+
+    // now it should just get the user
+    const result = await getOrMakeUser({ email: user.email.toUpperCase() });
+    expect(result).toEqual(userRow);
+  });
 });
