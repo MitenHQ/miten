8f74f8405e45e069822931ca425b2b56d6391e71
Pouria Maleki <dev.maleki@gmail.com>
2021-02-28 22:19:39 +0200

    remove copy-pasted code :D

--- backend/src/feedback/report.ts
+++ backend/src/feedback/report.ts
@@ -5,9 +5,6 @@ import {
 } from '../graphql/resolvers-types';
 import { makeFeedbackLink, makeReportLink } from './utils/makeLinks';
 
-export const DAY = 24 * 60; // a day is 24h * 60min
-export const GENERATE_LINK_DAY_LIMIT = 3;
-
 export const report: QueryResolvers['report'] = async (
   parent,
   { reportUid },
