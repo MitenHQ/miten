7da53f80adc6892ad0f9f4865da980c08074cc95
albacoretuna <7697632+albacoretuna@users.noreply.github.com>
2021-03-04 12:15:29 +0200

    Fix report url

--- backend/src/feedback/utils/makeLinks.ts
+++ backend/src/feedback/utils/makeLinks.ts
@@ -1,4 +1,4 @@
 export const makeFeedbackLink = (feedbackUid: string): string =>
   `${process.env.CLIENT_ADDRESS}/feedback/${feedbackUid}`;
 export const makeReportLink = (reportUid: string): string =>
-  `${process.env.CLIENT_ADDRESS}/feedback/${reportUid}`;
+  `${process.env.CLIENT_ADDRESS}/report/${reportUid}`;
