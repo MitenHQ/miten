266f083a0d2c577426f401c9be3133244cb42351
Pouria Maleki <dev.maleki@gmail.com>
2021-03-06 15:40:02 +0200

    fix the build

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,7 +3,7 @@ import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useApollo } from '../lib/apolloClient';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { theme } from './theme';
+import { theme } from '../lib/theme';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps);
