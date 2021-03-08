aaeb7d0404747cd36927197bee8169b37156946d
albacoretuna <7697632+albacoretuna@users.noreply.github.com>
2021-03-08 20:23:19 +0200

    Hide the email form after submit

--- frontend/components/Home/EmailForm.tsx
+++ frontend/components/Home/EmailForm.tsx
@@ -2,26 +2,18 @@ import React, { FC } from 'react';
 import { useGenerateLinkMutation } from '../../lib/graphql/hooks';
 import {
   Button,
+  Collapse,
   Input,
   FormControl,
   FormHelperText,
   FormLabel,
   SimpleGrid,
+  Text,
 } from '@chakra-ui/react';
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { emailRegex } from '../utils/constants';
 import { getMessage } from './getMessage';
-
-const MessageContainer = styled.section`
-  display: flex;
-  flex-flow: column nowrap;
-  margin-top: 20px;
-  color: teal;
-  & span {
-    font-size: 0.8rem;
-  }
-`;
 
 type FormValues = {
   email: string;
@@ -40,36 +32,38 @@ export const EmailForm: FC = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(submit)}>
-        <FormControl id="email">
-          <SimpleGrid spacing="10px">
-            <FormLabel mb="0">Your email</FormLabel>
-            <Input
-              type="email"
-              name="email"
-              ref={register({
-                required: 'Email is required to register',
-                pattern: {
-                  message: 'Email is not entered correctly',
-                  value: emailRegex,
-                },
-              })}
-            />
-            <Button
-              type="submit"
-              isLoading={loading}
-              loadingText="Generating..."
-              colorScheme="teal"
-            >
-              Get a feedback link!
-            </Button>
-            <FormHelperText>{'We will never share your email.'}</FormHelperText>
-          </SimpleGrid>
-        </FormControl>
-      </form>
-      <MessageContainer>
-        <span>{getMessage(data, error)}</span>
-      </MessageContainer>
+      <Collapse startingHeight={0} in={!data}>
+        <form onSubmit={handleSubmit(submit)}>
+          <FormControl id="email">
+            <SimpleGrid spacing="10px">
+              <FormLabel mb="0">Your email</FormLabel>
+              <Input
+                type="email"
+                name="email"
+                ref={register({
+                  required: 'Email is required to register',
+                  pattern: {
+                    message: 'Email is not entered correctly',
+                    value: emailRegex,
+                  },
+                })}
+              />
+              <Button
+                type="submit"
+                isLoading={loading}
+                loadingText="Generating..."
+                colorScheme="teal"
+              >
+                Get a feedback link!
+              </Button>
+              <FormHelperText>{'We will never share your email.'}</FormHelperText>
+            </SimpleGrid>
+          </FormControl>
+        </form>
+      </Collapse>
+      <Text fontSize="2xl" color="teal" mt={4}>
+        {getMessage(data, error)}
+      </Text>
     </>
   );
 };
