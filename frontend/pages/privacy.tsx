22f496342f0aa163ba93d61892345d3af3d3504e
Pouria Maleki <dev.maleki@gmail.com>
2021-04-12 21:15:49 +0300

    privacy page styles

--- frontend/pages/privacy.tsx
+++ frontend/pages/privacy.tsx
@@ -7,6 +7,31 @@ import { useTranslation } from 'next-i18next';
 import { Layout } from '../components/Layout';
 import { gql } from '@apollo/client';
 import { createContentfulGrapqlQLClient } from '../lib/contentfulClient';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: auto;
+  margin-top: 16px;
+  margin-bottom: 32px;
+
+  h3 {
+    font-size: 18px;
+    font-weight: 500;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  ul,
+  ol {
+    margin: 0 32px;
+  }
+
+  a {
+    border-bottom: 1px solid #319795;
+    color: #319795;
+    cursor: pointer;
+  }
+`;
 
 type Props = {
   content: string;
@@ -17,7 +42,9 @@ const HomePage: FC<Props> = (props) => {
 
   return (
     <Layout title={t('Miten - Privacy Policy')}>
-      <ReactMarkdown>{props.content || ''}</ReactMarkdown>
+      <Wrapper>
+        <ReactMarkdown>{props.content || ''}</ReactMarkdown>
+      </Wrapper>
     </Layout>
   );
 };
